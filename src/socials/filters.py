ca0c75e565ab230a128a4ea9c769ed4631ba5279
Viktor Bazar <viktor.bsns@gmail.com>
2024-05-05 00:19:55 +0300

    fix: change condition for instagram trigger

--- src/socials/filters.py
+++ src/socials/filters.py
@@ -1,10 +1,13 @@
-from re import findall
 from typing import Any
 
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
+from src.socials.services import InstagramService
+
 
 class IsInstagram(BaseFilter):
     async def __call__(self, message: Message) -> Any:
-        return findall(r"instagram.com\/.+", message.text)  # type: ignore
+        serv = InstagramService()
+        result = serv._get_shortcode(message.text)
+        return bool(result)
